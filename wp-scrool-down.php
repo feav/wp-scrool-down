02aa8c1c718bca48d2caf0bea12503529398818e
Armel E <armelem@ausiteodit.com>
2019-09-11 17:28:01 +0100

    ajout de parametres

--- wp-scrool-down.php
+++ wp-scrool-down.php
@@ -50,7 +50,15 @@ class WpScrollDown {
                     'name' =>__(  "SCROLL DOWN ", 'js_composer'  ),
                     'class' => '',
                     'icon' =>'iw-default',// 'icon-heart',
-                    'params' => array(
+                    'params' => array(array(
+                          "type" => "colorpicker",
+                          "holder" => "Couleur du widget",
+                          "class" => "",
+                          "heading" => "Couleur",
+                          "param_name" => "couleur",
+                          "value" => __( "#fff", "my-text-domain" ),
+                          "description" => "La couleur pour personnaliser le kit" 
+                        )
                     )
                 ) 
             );
@@ -61,44 +69,36 @@ class WpScrollDown {
      * SHORCODE INTEGRATION
      */
     function get_form_scroll($atts = [], $content = null, $tag = ''){
+        // normalize attribute keys, lowercase
+        $atts = array_change_key_case((array)$atts, CASE_LOWER);
+            // override default attributes with user attributes
+         $wporg_atts = shortcode_atts(
+            [
+                'kecouleury'=>  "#fff"
+            ], $atts, $tag);
+         $couleur = $wporg_atts['couleur'];
         if ( is_admin() ) {
                 echo "Integration Bon de visite ";
         } else {
-        
-             ob_start();
-    ?>
-
-            <link rel="stylesheet" href="<?php echo WSD_URL;?>template/css/style.css">
-            <div class=" wp-scroll-down bk-scroller">
-                <div class="scroller_set _element">
-                    <div class="scroller_shape">
-                        <svg height="20" width="10">
-                            <circle class="scroller-c1" cx="5" cy="15" r="2"></circle>
-                            <circle class="scroller-c2" cx="5" cy="15" r="2"></circle>
-                        </svg>
-                    </div>
-                </div>
-                <div class="text-scroll">
-                    <!-- <a href="javascript:void()"> SCROLLEZ POUR EN SAVOIR PLUS </a>     -->
-                </div>  
-            </div>
-            <style type="text/css">
-                .scroller_set {
-                    width: 25px;
-                    height: 40px;
-                }
-                .scroller_shape {
-                  border: 3px solid #000;
-                  border-radius: 50px!important;
-                  -webkit-border-radius: 50px!important;
-                }
-                .scroller-c1, .scroller-c2 {
-                    fill: #000;
-                }
-            </style>
-    <?php
-             $message =  ob_get_contents() ;
-             ob_end_clean();
+            $message =  "<link rel='stylesheet' href='".WSD_URL."template/css/style.css'>".
+            "<div class='wp-scroll-down bk-scroller'>".
+                "<div class='scroller_set _element'>".
+                    "<div class='scroller_shape'>".
+                        "<svg height='20' width='10'>".
+                            "<circle class='scroller-c1' cx='5' cy='15' r='2'></circle>".
+                            "<circle class='scroller-c2' cx='5' cy='15' r='2'></circle>".
+                        "</svg>".
+                    "</div>".
+                "</div>".
+                "<div class='text-scroll'>".
+                    "<!-- <a href='javascript:void()'> SCROLLEZ POUR EN SAVOIR PLUS </a>     -->".
+                "</div>  ".
+            "</div>".
+            "<style type='text/css'>".
+                ".scroller_set {width: 25px;height: 40px;}".
+                ".scroller_shape {border: 3px solid #fff;border-radius: 50px!important;-webkit-border-radius:50px !important;}".
+               ".scroller-c1, .scroller-c2 {fill: #fff;}".
+            "</style>";
              echo $message;
         }
     }
